4090a0133c721dd5f77c68b42fb1d4e61e758358
G4me4u <G4me4u@gmail.com>
2021-08-18 16:04:42 +0200

    Added Correct Entity Pushing setting to correct pistons pushing logic on the client, such that it respects piston animation type.

---
+++ src/main/java/com/g4mesoft/access/client/GSIMouseAccess.java
@@ -0,0 +1,9 @@
+package com.g4mesoft.access.client;
+
+public interface GSIMouseAccess {
+
+	public int getPreviousEventModifiers();
+
+	public double getPreviousEventScrollX();
+	
+}
---
+++ src/main/java/com/g4mesoft/access/client/GSIWorldRendererAccess.java
@@ -0,0 +1,9 @@
+package com.g4mesoft.access.client;
+
+import net.minecraft.util.math.BlockPos;
+
+public interface GSIWorldRendererAccess {
+
+	public void scheduleBlockUpdate0(BlockPos pos, boolean important);
+	
+}
