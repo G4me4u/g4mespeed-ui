11fb4895fa6a73472b9d2efc608d5de931d9edf1
G4me4u <G4me4u@gmail.com>
2023-03-21 01:43:04 +0100

    Added registration of 3d renderables

--- src/main/java/com/g4mesoft/mixin/client/GSWorldRendererMixin.java
+++ src/main/java/com/g4mesoft/mixin/client/GSWorldRendererMixin.java
@@ -3,7 +3,6 @@ package com.g4mesoft.mixin.client;
 import java.util.Collection;
 
 import org.lwjgl.opengl.GL11;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,15 +10,10 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.g4mesoft.access.client.GSIMinecraftClientAccess;
-import com.g4mesoft.access.client.GSIWorldRendererAccess;
-import com.g4mesoft.core.client.GSClientController;
-import com.g4mesoft.module.tps.GSTpsModule;
+import com.g4mesoft.G4mespeedUIMod;
 import com.g4mesoft.renderer.GSBasicRenderer3D;
 import com.g4mesoft.renderer.GSERenderPhase;
 import com.g4mesoft.renderer.GSIRenderable3D;
@@ -27,31 +21,20 @@ import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.BufferBuilderStorage;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.Tessellator;
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Matrix4f;
 
 @Mixin(WorldRenderer.class)
-public abstract class GSWorldRendererMixin implements GSIWorldRendererAccess {
+public abstract class GSWorldRendererMixin {
 
 	@Shadow @Final private MinecraftClient client;
 	
-	@Shadow protected abstract void scheduleSectionRender(BlockPos pos, boolean important);
-	
-	@Unique
-	private GSClientController gs_controller;
-	@Unique
-	private GSTpsModule gs_tpsModule;
 	@Unique
 	private GSBasicRenderer3D gs_renderer3d;
 
@@ -60,8 +43,6 @@ public abstract class GSWorldRendererMixin implements GSIWorldRendererAccess {
 		at = @At("RETURN")
 	)
 	private void onInit(MinecraftClient client, BufferBuilderStorage builderStorage, CallbackInfo ci) {
-		gs_controller = GSClientController.getInstance();
-		gs_tpsModule = gs_controller.getTpsModule();
 		gs_renderer3d = new GSBasicRenderer3D();
 	}
 	
@@ -121,7 +102,7 @@ public abstract class GSWorldRendererMixin implements GSIWorldRendererAccess {
 
 	@Unique
 	private void handleOnRenderTransparentLast(MatrixStack matrixStack) {
-		Collection<GSIRenderable3D> renderables = gs_controller.getRenderables();
+		Collection<GSIRenderable3D> renderables = G4mespeedUIMod.getRenderables();
 		
 		if (hasRenderPhase(renderables, GSERenderPhase.TRANSPARENT_LAST)) {
 			// Rendering world border sometimes has depth and
@@ -169,53 +150,5 @@ public abstract class GSWorldRendererMixin implements GSIWorldRendererAccess {
 		}
 
 		return false;
-	}
-	
-	@ModifyArg(
-		method = "render",
-		index = 4,
-		at = @At(
-			value = "INVOKE", 
-			target =
-				"Lnet/minecraft/client/render/WorldRenderer;renderEntity(" +
-					"Lnet/minecraft/entity/Entity;" +
-					"DDDF" +
-					"Lnet/minecraft/client/util/math/MatrixStack;" +
-					"Lnet/minecraft/client/render/VertexConsumerProvider;" +
-				")V"
-		)
-	)
-	private float onRenderEntityModifyDeltaTick(Entity entity, double cameraX, double cameraY, double cameraZ, float deltaTick, MatrixStack matrices, VertexConsumerProvider vertexConsumers) {
-		if (!client.isPaused() && (entity instanceof AbstractClientPlayerEntity)) {
-			if (gs_tpsModule.isPlayerFixedMovement((AbstractClientPlayerEntity)entity))
-				return ((GSIMinecraftClientAccess)client).gs_getFixedMovementTickDelta();
-		}
-
-		return deltaTick;
-	}
-	
-	@Redirect(
-		method = "render",
-		allow = 1,
-		require = 1,
-		expect = 1,
-		at = @At(
-			value = "FIELD",
-			opcode = Opcodes.GETFIELD,
-			target="Lnet/minecraft/entity/Entity;age:I"
-		)
-	)
-	private int onRenderGetEntityAge(Entity entity) {
-		if (gs_tpsModule.sPrettySand.get() != GSTpsModule.PRETTY_SAND_DISABLED && entity.getType() == EntityType.FALLING_BLOCK) {
-			// We do not want the render positions to be modified when
-			// using pretty sand (already done by position packets).
-			return (entity.age == 0) ? -1 : entity.age;
-		}
-		return entity.age;
-	}
-	
-	@Override
-	public void gs_scheduleBlockUpdate(BlockPos pos, boolean important) {
-		scheduleSectionRender(pos, important);
 	}
 }
