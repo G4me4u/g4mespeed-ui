23cc111e5849f7d7c4c66ffc8fa4ffc1ca2c7149
G4me4u <G4me4u@gmail.com>
2025-04-21 19:05:07 +0200

    Simplified retrieval of width of text with no style

--- src/main/java/com/g4mesoft/ui/renderer/GSBasicRenderer2D.java
+++ src/main/java/com/g4mesoft/ui/renderer/GSBasicRenderer2D.java
@@ -13,7 +13,6 @@ import com.g4mesoft.ui.util.GSMathUtil;
 import com.mojang.blaze3d.systems.RenderSystem;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.FontStorage;
 import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.VertexFormat;
@@ -380,11 +379,12 @@ public class GSBasicRenderer2D implements GSIRenderer2D {
 
 	@Override
 	public float getTextWidthNoStyle(CharSequence text) {
-		float w = 0.0f;
-		FontStorage fontStorage = ((GSITextRendererAccess)client.textRenderer).getFontStorage();
-		for (int i = 0; i < text.length(); i++)
-			w += fontStorage.getGlyph(text.charAt(i)).getAdvance();
-		return (float)Math.ceil(w);
+		try {
+			((GSITextRendererAccess)client.textRenderer).setEscapeTextFlag(true);
+			return getTextWidth(text);
+		} finally {
+			((GSITextRendererAccess)client.textRenderer).setEscapeTextFlag(false);
+		}
 	}
 
 	@Override
